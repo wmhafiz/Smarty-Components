282322c2f331ffb18d2185760332a93ee258c4a7
Wan Mohd Hafiz <fizyboy@gmail.com>
2019-05-25 02:04:54 +0800

    filters & useAggregations

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -2,10 +2,11 @@ import React, { useContext } from "react";
 import {
   SmartyProvider,
   SmartyContext,
-  useQuery
+  useQuery,
+  useAggregation
 } from "../context/smarty-context";
 
-const Table = ({ data, columns }) => {
+const Table = ({ rows, columns, limit = 20 }) => {
   return (
     <table className="table table-striped table-bordered">
       <thead className="thead-dark">
@@ -19,31 +20,37 @@ const Table = ({ data, columns }) => {
         </tr>
       </thead>
       <tbody>
-        {data.results &&
-          data.results.map((row, i) => (
-            <tr key={i}>
-              {columns && columns.map(col => <td>{`${row[col.key]}`}</td>)}
-            </tr>
-          ))}
+        {rows &&
+          rows
+            .slice(0, limit)
+            .map((row, i) => (
+              <tr key={i}>
+                {columns && columns.map(col => <td>{`${row[col.key]}`}</td>)}
+              </tr>
+            ))}
       </tbody>
     </table>
   );
 };
 
-export const SearchResult = ({ entity, columns, keywordField }) => {
+export const SearchResult = ({ entity, columns, keywordField, ...props }) => {
   const { data, isLoading } = useQuery({
     entity,
     keywordField
   });
-  return isLoading ? <p>Loading..</p> : <Table data={data} columns={columns} />;
+  return isLoading ? (
+    <p>Loading..</p>
+  ) : (
+    <Table rows={data.results} columns={columns} {...props} />
+  );
 };
 
 export const Searchbar = () => {
   const { keyword, setKeyword } = useContext(SmartyContext);
   return (
-    <div class="md-form mt-0">
+    <div className="md-form mt-0">
       <input
-        class="form-control"
+        className="form-control"
         type="text"
         placeholder="Search"
         aria-label="Search"
@@ -54,57 +61,109 @@ export const Searchbar = () => {
   );
 };
 
+export const Filters = ({ entity, keywordField, keys, label, ...props }) => {
+  const { data, isLoading } = useAggregation({
+    entity,
+    keywordField,
+    keys
+  });
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <Table
+      rows={data}
+      columns={[
+        {
+          label,
+          key: "key1"
+        },
+        {
+          label: "Count",
+          key: "count"
+        }
+      ]}
+      {...props}
+    />
+  );
+};
+
 export const Search = () => {
   return (
     <SmartyProvider
       defaultKeyword={"KFC"}
       token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNEY3dmxHa0I3X0JrT1g5bVQwbjEiLCJ1c2VybmFtZSI6IndtaGFmaXoiLCJ1c2VyX3R5cGUiOiJ1c2VyIn0sImlhdCI6MTU1MzU3MTk1MCwiZXhwIjoxNTg1MTI5NTUwfQ.wz_s0ef7OkizBIztv_6MZp6Uaooapwd6xGukcyBwIEg"
     >
-      <Searchbar />
-      <SearchResult
-        entity="sme"
-        keywordField="company_name"
-        columns={[
-          {
-            label: "SME",
-            key: "company_name"
-          },
-          {
-            label: "City",
-            key: "city"
-          },
-          {
-            label: "State",
-            key: "state"
-          },
-          {
-            label: "Contact",
-            key: "contact"
-          }
-        ]}
-      />
-      <SearchResult
-        entity="poi"
-        keywordField="name"
-        columns={[
-          {
-            label: "POI",
-            key: "name"
-          },
-          {
-            label: "Category 1",
-            key: "desc1"
-          },
-          {
-            label: "Category 2",
-            key: "desc2"
-          },
-          {
-            label: "State",
-            key: "state"
-          }
-        ]}
-      />
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters
+              entity="poi"
+              label="State"
+              keywordField="name"
+              keys={["state"]}
+              limit={3}
+            />
+
+            <Filters
+              entity="poi"
+              label="City"
+              keywordField="name"
+              keys={["city"]}
+              limit={3}
+            />
+
+            <Filters
+              entity="poi"
+              label="Category1"
+              keywordField="name"
+              keys={["desc1"]}
+              limit={3}
+            />
+
+            <Filters
+              entity="poi"
+              label="Category2"
+              keywordField="name"
+              keys={["desc2"]}
+              limit={3}
+            />
+
+            <Filters
+              entity="poi"
+              label="Category3"
+              keywordField="name"
+              keys={["desc3"]}
+              limit={3}
+            />
+          </div>
+          <div className="col-9">
+            <Searchbar />
+            <br />
+            <SearchResult
+              entity="poi"
+              keywordField="name"
+              columns={[
+                {
+                  label: "POI",
+                  key: "name"
+                },
+                // {
+                //   label: "Category1",
+                //   key: "desc1"
+                // },
+                {
+                  label: "Category2",
+                  key: "desc2"
+                },
+                {
+                  label: "State",
+                  key: "state"
+                }
+              ]}
+            />
+          </div>
+        </div>
+      </div>
     </SmartyProvider>
   );
 };
--- src/context/smarty-context.jsx
+++ src/context/smarty-context.jsx
@@ -15,11 +15,12 @@ const SmartyProvider = props => {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNEY3dmxHa0I3X0JrT1g5bVQwbjEiLCJ1c2VybmFtZSI6IndtaGFmaXoiLCJ1c2VyX3R5cGUiOiJ1c2VyIn0sImlhdCI6MTU1MzU3MTk1MCwiZXhwIjoxNTg1MTI5NTUwfQ.wz_s0ef7OkizBIztv_6MZp6Uaooapwd6xGukcyBwIEg"
   );
   const [keyword, setKeyword] = useState(props.defaultKeyword);
+  const [filters, setFilters] = useState({});
 
-  const value = useMemo(() => ({ token, setToken, keyword, setKeyword }), [
-    keyword,
-    token
-  ]);
+  const value = useMemo(
+    () => ({ token, setToken, keyword, setKeyword, filters, setFilters }),
+    [keyword, filters, token]
+  );
   return <SmartyContext.Provider value={value} {...props} />;
 };
 
@@ -28,14 +29,15 @@ const useQuery = props => {
   if (!context) {
     throw new Error("useQuery must be used within SmartyProvider");
   }
+  const { token, keyword } = context;
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(false);
-  const { token, keyword } = context;
   const { entity, keywordField } = props;
 
   useEffect(() => {
     setIsLoading(true);
     const searchUrl = `${baseUrl}/${entity}?api_key=${token}&${keywordField}=${keyword}`;
+    // console.log("searchUrl", searchUrl, " for ", entity);
     // console.log(searchUrl);
     axios(searchUrl).then(result => {
       setData(result.data);
@@ -46,4 +48,28 @@ const useQuery = props => {
   return { data, isLoading };
 };
 
-export { SmartyProvider, SmartyContext, useQuery };
+const useAggregation = props => {
+  const context = useContext(SmartyContext);
+  if (!context) {
+    throw new Error("useQuery must be used within SmartyProvider");
+  }
+  const { token, keyword } = context;
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { entity, keywordField, keys } = props;
+
+  useEffect(() => {
+    setIsLoading(true);
+    const searchUrl = `${baseUrl}/${entity}/count?api_key=${token}&${keywordField}=${keyword}&group_by=${keys}`;
+    // console.log("searchUrl", searchUrl, " for ", entity);
+    axios(searchUrl).then(result => {
+      setData(result.data);
+      // console.log("data", result.data);
+      setIsLoading(false);
+    });
+  }, [keywordField, token, entity, keyword, keys]);
+
+  return { data, isLoading };
+};
+
+export { SmartyProvider, SmartyContext, useAggregation, useQuery };
